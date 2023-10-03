ac5dd49c13c765c9d50a660615ac60ef414658d6
Paul Hethmon <paul@hethmon.com>
2023-10-03 16:48:43 -0400

    remove retsproperties from project

--- src/main/java/us/ampre/rets/RetsApplication.java
+++ src/main/java/us/ampre/rets/RetsApplication.java
@@ -20,9 +20,6 @@ import java.net.MalformedURLException;
 @SpringBootApplication
 public class RetsApplication implements CommandLineRunner {
 
-	@Autowired
-	private RetsProperties retsProperties;
-
 	public static void main(String[] args) {
 
 		SpringApplication app = new SpringApplication(RetsApplication.class);
@@ -34,59 +31,6 @@ public class RetsApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		log.info("RETS Starting ...");
-
-		trulia();
 	}
 
-	public  void trulia() throws MalformedURLException {
-
-		//Create a RetsHttpClient (other constructors provide configuration i.e. timeout, gzip capability)
-		RetsHttpClient httpClient = new CommonsHttpClient();
-		RetsVersion retsVersion = RetsVersion.RETS_1_7_2;
-		String loginUrl = retsProperties.getLoginUrl();
-
-		//Create a RetsSession with RetsHttpClient
-		RetsSession session = new RetsSession(loginUrl, httpClient, retsVersion);
-
-		String username 			= retsProperties.getUsername();
-		String password 			= retsProperties.getPassword();
-		String userAgent 			= retsProperties.getUserAgent();
-		String userAgentPassword 	= retsProperties.getUserAgentPassword();
-
-		//Set method as GET or POST
-		session.setMethod("POST");
-		try {
-			//Login
-			session.login(username, password);
-		} catch (RetsException e) {
-			log.error("", e);
-			return;
-		}
-
-		try {
-			Metadata m = session.getMetadata();
-
-			MSystem system = session.getMetadata().getSystem();
-			log.info("System: {} / {}", system.getSystemID(), system.getSystemDescription());
-			for(MResource resource: system.getMResources()) {
-				log.info("  Resource: {} / {}", resource.getResourceID(), resource.getDescription());
-				for(MClass classification: resource.getMClasses()) {
-					log.info("    Class: {} / {}", classification.getClassName(), classification.getDescription());
-					for (MTable mTable : classification.getMTables()) {
-						log.info("      Table: {} / {}", mTable.getSystemName(), mTable.getStandardName());
-					}
-				}
-			}
-		} catch (RetsException e) {
-			log.error("Exception traversing metadata tree.", e);
-		} finally {
-			if(session != null) {
-				try {
-					session.logout();
-				} catch(RetsException e) {
-					log.error("Exception ending RETS session/logout.", e);
-				}
-			}
-		}
-	}
 }
